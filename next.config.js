ef87567b5d3e86db825f9677189030715d6e082f
DamianMuracciole <102468224+DamianMuracciole@users.noreply.github.com>
2024-03-20 20:29:54 -0300

    modificacion de estilos de las ventanas de lienzos en Hacer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
+    // experimental: {
+    //     appDir: true,
+    // },
     webpack: (config) => {
         config.externals = [...config.externals, "canvas", "jsdom"];
         return config;
